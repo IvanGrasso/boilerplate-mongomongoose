c9461b47865c8fc0401cfa2a22620729f9acd43a
Ivan Grasso <grassoivan@gmail.com>
2021-05-24 14:09:08 -0300

    create model

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+const Person = mongoose.model("Person", personSchema);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
