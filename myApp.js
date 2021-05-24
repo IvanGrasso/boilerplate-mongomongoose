88025ada29807b1e0b3e95af7761a64bf2d8f880
Ivan Grasso <grassoivan@gmail.com>
2021-05-24 18:18:02 -0300

    fix

--- myApp.js
+++ myApp.js
@@ -101,13 +101,12 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person.find({food:foodToSearch})
-      .sort({name:-1})
+      .sort({name:1})
       .limit(2)
       .select({age:0})
       .exec(function (err, data) {
-        if (err) return console.log(err);
-        done(null, data);
-  });
+          (err) ? done(err) : done(null, data);
+      });
 };
 
 /** **Well Done !!**
